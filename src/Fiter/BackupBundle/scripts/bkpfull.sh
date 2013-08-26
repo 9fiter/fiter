c82de969b26273f1fe498e269d516220c8fc6a97
root <root@debian6.(none)>
2013-08-26 08:27:43 +0200

    -

--- src/Fiter/BackupBundle/scripts/bkpfull.sh
+++ src/Fiter/BackupBundle/scripts/bkpfull.sh
@@ -5,8 +5,8 @@ fiter_uploads_path=$3
 mc_skin_path=$4
 file=$5
 
-echo tar -jcvf $bkp_path/temp/$file --exclude-from=$exclude $fiter_uploads_path $mc_skin_path
-tar -jcvf $bkp_path/temp/$file --exclude-from=$exclude $fiter_uploads_path $mc_skin_path
+echo tar jcvf $bkp_path/temp/$file --exclude-from=$exclude $fiter_uploads_path $mc_skin_path
+tar jcvf $bkp_path/temp/$file --exclude-from=$exclude $fiter_uploads_path $mc_skin_path
 
 mv $bkp_path/temp/$file $bkp_path/$file
 mv $bkp_path/temp/$file.log $bkp_path/$file.log
