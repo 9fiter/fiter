3982902b08d2cd2ea0e64f2ee1bc30a7942e9d16
root <root@debian6.(none)>
2013-08-26 08:05:39 +0200

    -

--- src/Fiter/BackupBundle/scripts/makebkpfull.sh
+++ src/Fiter/BackupBundle/scripts/makebkpfull.sh
@@ -4,4 +4,4 @@ installpath=$2
 
 #script                          1         2        3                   4             5       logfile
 echo "sh $installpath/bkpfull.sh $bkp_path $exclude $fiter_uploads_path $mc_skin_path $file > $bkp_path/temp/$file.log"
-sh $installpath/bkpfull.sh $bkp_path $exclude $fiter_uploads_path $mc_skin_path $file > $bkp_path/temp/$file.log 
+bash $installpath/bkpfull.sh $bkp_path $exclude $fiter_uploads_path $mc_skin_path $file > $bkp_path/temp/$file.log 
