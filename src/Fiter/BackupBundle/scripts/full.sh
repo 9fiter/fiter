1111eb25d425ed7ce9dd3eee7000ea1e672ce87b
root <root@debian6.(none)>
2013-08-27 04:35:32 +0200

    -

--- src/Fiter/BackupBundle/scripts/full.sh
+++ src/Fiter/BackupBundle/scripts/full.sh
@@ -5,9 +5,10 @@ file=${now}_fullbackup_fiter.tar.bz2
 #echo "tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img"
 #tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img
 
-nohup tar cvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 /home/juegos/minecraft/craftopia > /home/backup/tar.output.txt 2> /home/backup/tar.errors.txt < /dev/null &
+### este funciona
+#nohup tar cvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 /home/juegos/minecraft/craftopia > /home/backup/tar.output.txt 2> /home/backup/tar.errors.txt < /dev/null &
 
-#nohup tar ... > /tmp/tar.output.txt 2> /tmp/tar.errors.txt < /dev/null &'
+nohup tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img > /home/backup/tar.output.txt 2> /home/backup/tar.errors.txt < /dev/null &
 
 #echo "mv /home/backup/temp/$file /home/backup/$file"
 mv /home/backup/temp/$file /home/backup/$file
