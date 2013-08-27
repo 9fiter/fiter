68d18870fc60539d2e8e94322609e90697b49c54
root <root@debian6.(none)>
2013-08-27 05:41:26 +0200

    -

--- src/Fiter/BackupBundle/scripts/full.sh
+++ src/Fiter/BackupBundle/scripts/full.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-now=$(date +'%Y%m%d%H%M%S')
+now=$(date +'%Y-%m-%d:%H-%M-%S')
 file=${now}_fullbackup_fiter.tar.bz2
 
 nohup tar jcvf /home/backup/$file --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img > /home/backup/$file.output.log 2> /home/backup/$file.errors.log < /dev/null &
