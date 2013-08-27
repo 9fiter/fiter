5544f528a194fae45012bd68e59499b3e47d8331
root <root@debian6.(none)>
2013-08-27 04:12:44 +0200

    -

--- src/Fiter/BackupBundle/scripts/full.sh
+++ src/Fiter/BackupBundle/scripts/full.sh
@@ -5,7 +5,7 @@ file=${now}_fullbackup_fiter.tar.bz2
 #echo "tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img"
 #tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img
 
-tar cvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 /home/backup/pruebas
+tar cvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 /home/juegos/minecraft/craftopia
 
 #echo "mv /home/backup/temp/$file /home/backup/$file"
 mv /home/backup/temp/$file /home/backup/$file
