0a69085b375e26e47d896107d9aeb85be55e1da0
root <root@debian6.(none)>
2013-08-27 06:07:42 +0200

    -

--- src/Fiter/BackupBundle/scripts/mcfull.sh
+++ src/Fiter/BackupBundle/scripts/mcfull.sh
@@ -5,7 +5,6 @@ installpath=$2
 
 
 mc_path=$3
+mc_file=${now}_fullbackup_minecraft.tar.bz2
 
-$file=${now}_fullbackup_minecraft.tar.bz2
-
-nohup tar jcvf $bkp_path/$file --exclude-from=$installpath/exclude $mc_path > $bkp_path/$file.output.log 2> $bkp_path/$file.errors.log < /dev/null &
+nohup tar jcvf $bkp_path/$mc_file --exclude-from=$installpath/exclude $mc_path > $bkp_path/$mc_file.output.log 2> $bkp_path/$mc_file.errors.log < /dev/null &
