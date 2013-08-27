03e1d8cc02c956f146bcd45e3c97f8327de9e25b
root <root@debian6.(none)>
2013-08-27 03:29:58 +0200

    -

--- src/Fiter/BackupBundle/scripts/full.sh
+++ src/Fiter/BackupBundle/scripts/full.sh
@@ -5,7 +5,7 @@ file=${now}_fullbackup_fiter.tar.bz2
 #echo "tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img"
 #tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img
 
-tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 /home/backup/pruebas
+tar cvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 /home/backup/pruebas
 
 #echo "mv /home/backup/temp/$file /home/backup/$file"
 #mv /home/backup/temp/$file /home/backup/$file
