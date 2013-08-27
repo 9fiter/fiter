0a6cd18df99e0834f19b3b0080bcffb92c6c5aca
root <root@debian6.(none)>
2013-08-27 06:57:17 +0200

    -

--- src/Fiter/BackupBundle/scripts/makebkpdiff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-installpath=$2
-. $installpath/getParams.sh
-
-#script                          1         2        3                   4             5        logfile
-echo "bash $installpath/bkpdiff.sh $bkp_path $exclude $fiter_uploads_path $mc_skin_path $file2 > $bkp_path/temp/$file2.log"
-bash $installpath/bkpdiff.sh $bkp_path $exclude $fiter_uploads_path $mc_skin_path $file2 > $bkp_path/temp/$file2.log
