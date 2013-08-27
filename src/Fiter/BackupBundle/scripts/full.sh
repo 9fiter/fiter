814779da4b4fafeffa9fdbbe896c00952276f968
root <root@debian6.(none)>
2013-08-27 05:08:06 +0200

    -

--- src/Fiter/BackupBundle/scripts/full.sh
+++ src/Fiter/BackupBundle/scripts/full.sh
@@ -5,16 +5,4 @@ file=${now}_fullbackup_fiter.tar.bz2
 #echo "tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img"
 ##tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img
 
-### este funciona
-#nohup tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img > /home/backup/tar.output.txt 2> /home/backup/tar.errors.txt < /dev/null &
-
-
-nohup tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img > /home/backup/$file.output.txt 2> /home/backup/$file.errors.txt < /dev/null && mv /home/backup/temp/$file /home/backup/$file $$ mv /home/backup/temp/$file.erros.txt /home/backup/$file.erros.txt &
-
-
-
-
-#echo "mv /home/backup/temp/$file /home/backup/$file"
-mv /home/backup/temp/$file /home/backup/$file
-#echo "mv /home/backup/temp/$file.log /home/backup/$file.log"
-mv /home/backup/temp/$file.log /home/backup/$file.log
+nohup tar jcvf /home/backup/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img > /home/backup/logs/$file.output.log 2> /home/backup/logs/$file.errors.log < /dev/null &
