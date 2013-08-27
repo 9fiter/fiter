8bdd388fb12f8309efbbe987b82b149f22507ae0
root <root@debian6.(none)>
2013-08-27 03:21:56 +0200

    -

--- src/Fiter/BackupBundle/scripts/full.sh
+++ src/Fiter/BackupBundle/scripts/full.sh
@@ -2,16 +2,12 @@
 now=$(date +'%Y%m%d%H%M%S')
 file=${now}_fullbackup_fiter.tar.bz2
 
-echo "ls -la /home/backup"
-ls -la /home/backup
+#echo "tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img"
+#tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img
 
-echo "ls -la /home/backup/temp"
-ls -la /home/backup/temp
+tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 /home/backup/pruebas
 
-echo "tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img"
-tar jcvf /home/backup/temp/${now}_fullbackup_fiter.tar.bz2 --exclude-from=/home/web/fiter/src/Fiter/BackupBundle/scripts/exclude /home/web/fiter/web/uploads /home/web/fiter/web/mcskin/img
-
-echo "mv /home/backup/temp/$file /home/backup/$file"
-mv /home/backup/temp/$file /home/backup/$file
-echo "mv /home/backup/temp/$file.log /home/backup/$file.log"
-mv /home/backup/temp/$file.log /home/backup/$file.log
+#echo "mv /home/backup/temp/$file /home/backup/$file"
+#mv /home/backup/temp/$file /home/backup/$file
+#echo "mv /home/backup/temp/$file.log /home/backup/$file.log"
+#mv /home/backup/temp/$file.log /home/backup/$file.log
