1954dc7bf26060a150194126af95a84cd1e0d675
root <root@debian6.(none)>
2013-08-27 05:57:18 +0200

    -

--- src/Fiter/BackupBundle/scripts/diff.sh
+++ src/Fiter/BackupBundle/scripts/diff.sh
@@ -3,4 +3,6 @@
 installpath=$2
 . $installpath/getParams.sh
 
-nohup tar jcvf $bkp_path/$file2 --exclude-from=$installpath/exclude $fiter_uploads_path $mc_skin_path -N $(date +"%Y-%m-01 00:00:00") > $bkp_path/$file2.output.log 2> $bkp_path/$file2.errors.log < /dev/null &
+fecha=$(date +"%Y-%m-01 00:00:00")
+
+nohup tar jcvf $bkp_path/$file2 --exclude-from=$installpath/exclude $fiter_uploads_path $mc_skin_path -N $fecha > $bkp_path/$file2.output.log 2> $bkp_path/$file2.errors.log < /dev/null &
