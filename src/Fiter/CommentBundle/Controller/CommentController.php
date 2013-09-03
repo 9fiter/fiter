43efd4ba2d8d0d3a64041e8b54ed7ed85155c448
root <root@debian6.(none)>
2013-09-03 09:45:55 +0200

    -

--- src/Fiter/CommentBundle/Controller/CommentController.php
+++ src/Fiter/CommentBundle/Controller/CommentController.php
@@ -40,4 +40,48 @@ class CommentController extends Controller{
 
 
 
+
+
+
+    /**
+     * Returns a flat array of last comments.
+     * @Route("/index", name="fos_comment_index")
+     * @Template()
+     */
+    public function indexAction(){
+    	$em = $this->getDoctrine()->getManager();
+
+        $paginador = $this->get('ideup.simple_paginator');
+        $paginador->setItemsPerPage(50);
+        $paginador->setMaxPagerItems(10);
+
+        $entities = 
+        $paginador->paginate(
+            $query = $em->createQuery('SELECT p FROM FiterDefaultBundle:Comment p ORDER BY p.createdAt DESC')
+        )->getResult();
+        if (!$entities) throw $this->createNotFoundException('No se ha encontrado ningun resultado');
+
+        $res = array();
+        foreach ($entities as $key => $comment) {
+        	$id=\str_replace("type_", "",$comment->getThread()->getId());
+        	$articulo = $em->getRepository('FiterDefaultBundle:Articulo')->find($id);
+        	//ladybug_dump($articulo);
+        	//ladybug_dump($comment);
+        	$res[]= array(
+			    "comment" => $comment,
+			    "articulo" => $articulo,
+			);
+        }
+
+        return array(
+        	'res' => $res,
+            'entities' => $entities,
+            'paginador' => $paginador
+        );
+        
+    }
+
+
+
+
 }
